fefb621c9e600de0b1d51eaa9782989e0e5e9a78
STUDENT Youenn Baton <Youenn.Baton@students.ittralee.ie>
2024-12-11 13:06:51 +0000

    Signed-off-by: STUDENT Youenn Baton <Youenn.Baton@students.ittralee.ie>

--- RadX/Assets/EdScript.cs
+++ RadX/Assets/EdScript.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class EdScript : MonoBehaviour
 {
     public GameObject giftCloneTemplate;
+    bool hasTurned = false;
     float turningSpeed = 90;
     float speed;
     float walkingSpeed = 1;
     float runningMultiplier = 3;
     Animator edanim;
+    Transform camera;
+    float giftHeight = 0.5f, giftForward = 0.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        camera = Camera.main.transform;
         edanim = GetComponent<Animator>();
         speed = 0;
     }
@@ -47,22 +51,53 @@ public class EdScript : MonoBehaviour
         }
             if (Input.GetKey(KeyCode.A))
         {
-            // Roll Left
-            transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
-            edanim.SetBool("IsTurning", true) ;
+            if (!hasTurned)
+            {
+                camera.parent = null;
+                transform.Rotate(Vector3.up, -90);
+                hasTurned = true;
+            }
+            
+            speed = walkingSpeed;
+            edanim.SetBool("IsWalking", true);
+            //transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
+            camera.parent = transform;
+            //edanim.SetBool("IsTurning", true) ;
+
         }
         if (Input.GetKey(KeyCode.D))
         {
-            // Pitch Up
-            transform.Rotate(Vector3.up, -turningSpeed * Time.deltaTime);
-            edanim.SetBool("IsTurning", true);
+            if (!hasTurned)
+            {
+                camera.parent = null;
+                transform.Rotate(Vector3.up, 90);
+                hasTurned = true;
+            }
+
+            speed = walkingSpeed;
+            edanim.SetBool("IsWalking", true);
+            //transform.Rotate(Vector3.up, turningSpeed * Time.deltaTime);
+            camera.parent = transform;
+            //edanim.SetBool("IsTurning", true) ;
+
+
+            //camera.parent = null;
+            //transform.Rotate(Vector3.up, -turningSpeed * Time.deltaTime);
+            //camera.parent = transform;
+            //edanim.SetBool("IsTurning", true);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(giftCloneTemplate,transform.position,transform.rotation);
+           GameObject newGiftGO =  Instantiate(giftCloneTemplate,transform.position + giftForward * transform.forward +giftHeight *transform.up,transform.rotation);
+           GiftProjectileScript newGift = newGiftGO.GetComponent<GiftProjectileScript>();
+            newGift.launchMe(transform.forward);
         }
         transform.position += speed * transform.forward * Time.deltaTime;
+
+
+        hasTurned = (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D));
+        
     }
 
 }
---
+++ RadX/Assets/GiftProjectileScript.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftProjectileScript : MonoBehaviour
+{
+    Rigidbody rb;
+
+    float force = 20f;
+
+    float jumpforce = 10;
+    float explosionRadius = 1;
+    float explosionStrength = 10000;
+
+    internal void launchMe(Vector3 dir)
+    {
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        rb.AddForce(force*dir, ForceMode.Impulse);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        VictimScript victimScript = collision.gameObject.GetComponent<VictimScript>();
+
+        Collider[] allVictims = Physics.OverlapSphere(transform.position + Vector3.down, explosionRadius);
+
+        foreach (Collider collider in allVictims)
+        {
+            VictimScript newVictim = collider.gameObject.GetComponent<VictimScript>();
+
+            if (newVictim != null)
+            {
+                newVictim.Bump(explosionStrength, transform.position + Vector3.down, explosionRadius);
+            }
+        }
+
+        if (victimScript != null)
+        {
+            victimScript.Bump(explosionStrength, transform.position + Vector3.down, explosionRadius);
+
+        }
+
+    }
+}
